4d22afef3fd3b99126ecf23b464fdf994765e471
KristyKostyor <ms.kostyor@gmail.com>
2023-05-13 17:52:28 +0300

    fixed

--- 1.js
+++
@@ -1,9 +0,0 @@
-const cats = () => {
-  const nickName = ["Shreda", "Whiskas", "Tom", "Jimmy", "Charles"];
-  const cuteOftheCats = () => {
-    return [nickName = Math.floor(Math.random() * nickName.length)];
-  };
-  return cuteOftheCats;
-};
-const cutyCat = cats();
-console.log(cutyCat());
--- script.js
+++ script.js
@@ -6,49 +6,64 @@ const gameModule = (() => {
     return number % 2 === 0;
   };
 
-  const play = () => {
-    while (playerBalls > 0 && botBalls > 0) {
-      const playerGuess = parseInt(
-        prompt(
-          `У вас ${playerBalls} шариков. Введите число от 1 до ${playerBalls}:`
-        )
-      );
+const play = () => {
+  while (playerBalls > 0 && botBalls > 0) {
+    const maxGuess = Math.min(playerBalls, botBalls);
+    let playerGuess = prompt(
+      `У вас ${playerBalls} шариков. Введите число от 1 до ${maxGuess}:`
+    );
 
-      if (playerGuess < 1 || playerGuess > playerBalls) {
-        alert("Некорректный ввод!");
-        continue;
-      }
+    if (playerGuess === null) {
+      alert("Игра завершена!");
+      break;
+    }
 
-      if (Math.random() < 0.5) {
-        const botGuess = playerGuess;
-        alert(`Бот угадал! Он забирает ${botGuess} ваших шариков.`);
-        playerBalls -= botGuess;
-        botBalls += botGuess;
+    playerGuess = parseInt(playerGuess);
+
+    if (isNaN(playerGuess) || playerGuess < 1 || playerGuess > maxGuess) {
+      alert("Некорректный ввод!");
+      continue;
+    }
+
+    if (playerGuess > botBalls) {
+      alert("Вы не можете выбрать больше шариков, чем есть у бота!");
+      continue;
+    }
+
+    if (Math.random() < 0.5) {
+      const botGuess = playerGuess;
+      alert(`Бот угадал! Он забирает ${botGuess} ваших шариков.`);
+      playerBalls -= botGuess;
+      botBalls += botGuess;
+    } else {
+      const botGuess = Math.floor(Math.random() * (maxGuess - 1)) + 1;
+      if (botGuess === playerGuess) {
+        alert(`Бот угадал! Он забирает ${playerGuess} шариков.`);
+        playerBalls -= playerGuess;
+        botBalls += playerGuess;
       } else {
-        const botGuess = Math.floor(Math.random() * (playerBalls - 1)) + 1;
-        if (botGuess === playerGuess) {
-          alert(`Бот угадал! Он забирает ${playerGuess} шариков.`);
-          playerBalls -= playerGuess;
-          botBalls += playerGuess;
-        } else {
-          const botBallsToGive = playerGuess;
-          playerBalls += botBallsToGive;
-          botBalls -= botBallsToGive;
-          alert(`Бот не угадал. Он отдает ${botBallsToGive} ваших шариков.`);
-        }
-      }
-
-      if (playerBalls === 0) {
-        alert("Вы проиграли! У вас закончились шарики.");
-      } else if (botBalls === 0) {
-        alert("Вы выиграли! У бота закончились шарики.");
-      } else {
-        alert(
-          `У вас осталось ${playerBalls} шариков и у бота ${botBalls} шариков.`
-        );
+        const botBallsToGive = playerGuess;
+        playerBalls += botBallsToGive;
+        botBalls -= botBallsToGive;
+        alert(`Бот не угадал. Он отдает ${botBallsToGive} своих шарика(ов).`);
       }
     }
-  };
+
+    if (playerBalls === 0) {
+      alert("Вы проиграли! У вас закончились шарики.");
+    } else if (botBalls === 0) {
+      alert("Вы выиграли! У бота закончились шарики.");
+    } else {
+      alert(
+        `У вас осталось ${playerBalls} шариков и у бота ${botBalls} шариков.`
+      );
+    }
+
+    if (botBalls < 0) {
+      botBalls = 0;
+    }
+  }
+};
 
   return {
     play,
